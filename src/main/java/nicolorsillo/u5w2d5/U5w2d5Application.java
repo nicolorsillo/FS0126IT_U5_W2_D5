57703837a04b975ae3f08670c6cc1a0f68519d5d
nicolorsillo <nicolorsillo@hotmail.it>
2026-07-09 22:35:59 +0200

    Initial commit

---
+++ src/main/java/nicolorsillo/u5w2d5/U5w2d5Application.java
@@ -0,0 +1,13 @@
+package nicolorsillo.u5w2d5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5w2d5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5w2d5Application.class, args);
+	}
+
+}
